bbc2109dcffca6192e01b435c6970c258a4e4207
al_al_blyat <77665875+avmal0-Cor@users.noreply.github.com>
2022-06-27 17:41:51 +0300

    initial commit

---
+++ code/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stalker
+{
+	internal class Game
+	{
+		// TODO: clean repo ASAP 
+		void World() { }
+		void AI () { }
+		void Entity () { }
+		void Logic () { }
+		void Main(string[] args) { }
+	}
+}
